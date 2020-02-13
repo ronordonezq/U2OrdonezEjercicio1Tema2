e727366f5894dfd47ee80946111a668504452a61
Ronald Ordoñez <37679541+ronordonezq@users.noreply.github.com>
2020-02-13 12:35:42 -0500

    Agregacion de AsyncTask en InsertarCliente

--- app/src/main/java/com/example/u2tema1android/InsertarCliente.java
+++ app/src/main/java/com/example/u2tema1android/InsertarCliente.java
@@ -2,6 +2,8 @@ package com.example.u2tema1android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -29,7 +31,8 @@ public class InsertarCliente extends AppCompatActivity {
     private EditText direccion;
     private int isexo;
     HttpURLConnection conexion;
-
+    private URL url;
+    private JSONObject postData = new JSONObject();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,73 +47,59 @@ public class InsertarCliente extends AppCompatActivity {
 
 
     public void Insertar(View view){
-        //GET
-        /*try {
-            String miurl= this.getString(R.string.dominio)+this.getString(R.string.insertarcliente)
-                    + "nombre="+ URLEncoder.encode(nombre.getText().toString(),"UTF-8") + "&apellido="
-                    + URLEncoder.encode(apellido.getText().toString(), "UTF-8") + "&sexo=" + isexo+"&telefono="
-                    + nombre.getText().toString()+ "&telefono="+ URLEncoder.encode(telefono.getText().toString(), "UTF-8")
-                    + "&direccion="+ URLEncoder.encode(direccion.getText().toString(), "UTF-8");
-            URL url=new URL(miurl);
-            Log.e("mierror",miurl);
-            conexion = (HttpURLConnection) url
-                    .openConnection();
-            if (conexion.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(conexion.getInputStream()));
-                String linea = reader.readLine();
-                if (!linea.equals("OK\\n")) {
-                    Log.e("mierror","Error en servicio Web nueva");
-                }
-                else
-                    Toast.makeText(this, "Inserción exitosa", Toast.LENGTH_LONG).show();
-                finish();
-                {Log.e("mierror","No hay error");}
-            } else {
-                Log.e("mierror", conexion.getResponseMessage());
-            }
-        } catch (Exception e) {
-            Log.e("mierror", e.getMessage(), e);
-        } finally {
-            if (conexion!=null) conexion.disconnect();
-        }*/
-
         //POST
         try {
-            JSONObject postData = new JSONObject();
             postData.put("nombre", nombre.getText().toString());
             postData.put("apellido", apellido.getText().toString());
             postData.put("sexo", isexo);
             postData.put("telefono", telefono.getText().toString());
             postData.put("direccion", direccion.getText().toString());
-            String myurl= this.getString(R.string.dominio)+this.getString(R.string.insertarclientepost);
-            Log.i("respuesta",myurl);
-            URL url = new URL(myurl);
-            conexion = (HttpURLConnection) url.openConnection();
-            conexion.setRequestProperty("Content-Type", "application/json");
-            conexion.setRequestMethod("POST");
-            conexion.setDoOutput(true);
-            conexion.setDoInput(true);
-            conexion.setChunkedStreamingMode(0);
-            OutputStream out = new BufferedOutputStream(conexion.getOutputStream());
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
-                    out, "UTF-8"));
-            writer.write(postData.toString());
-            writer.flush();
-            if (conexion.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(conexion.getInputStream()));
-                String linea = reader.readLine();
-                if (!linea.equals("OK\\n")) Log.e("mierror","Error en servicio Web nueva");
-                else
-                { Toast.makeText(this, "Inserción Exitosa", Toast.LENGTH_SHORT).show();
-                    finish();
-                    Log.e("mierror","No hay error");}
-            } else {Log.e("mierror", conexion.getResponseMessage());}
+
+            AsyncInsertar insertarAsync = new AsyncInsertar();
+            insertarAsync.execute();
+            Toast.makeText(InsertarCliente.this, "Inserción Exitosa", Toast.LENGTH_LONG).show();
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             if (conexion != null) {
                 conexion.disconnect();
             }
+        }
+    }
+
+    class AsyncInsertar extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                String myurl= InsertarCliente.this.getString(R.string.dominio)+InsertarCliente.this.getString(R.string.insertarclientepost);
+                Log.i("respuesta",myurl);
+                url = new URL(myurl);
+                conexion = (HttpURLConnection) url.openConnection();
+                conexion.setRequestProperty("Content-Type", "application/json");
+                conexion.setRequestMethod("POST");
+                conexion.setDoOutput(true);
+                conexion.setDoInput(true);
+                conexion.setChunkedStreamingMode(0);
+                OutputStream out = new BufferedOutputStream(conexion.getOutputStream());
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
+                        out, "UTF-8"));
+                writer.write(postData.toString());
+                writer.flush();
+                if (conexion.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(conexion.getInputStream()));
+                    String linea = reader.readLine();
+                    if (!linea.equals("OK\\n")) Log.e("mierror","Error en servicio Web nueva");
+                    else
+                    {
+                        finish();
+                        Log.e("mierror","No hay error");}
+                } else {Log.e("mierror", conexion.getResponseMessage());}
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+            }
+            return null;
         }
     }
 }
